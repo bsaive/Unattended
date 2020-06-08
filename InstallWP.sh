b18e894ff564e4e99cf5d135902df10a7f296d9e
Pôm <ben@saive.cc>
2020-06-08 22:26:42 +0200

    Added Nginx config

--- InstallWP.sh
+++ InstallWP.sh
@@ -23,22 +23,44 @@ certbot --nginx certonly -d $domain -d www.$domain
 
 echo "Creating NGINX configuration file"
 cat > /etc/nginx/sites-available/$domain <<EOF
+# Define path to cache and memory zone. The memory zone should be unique.
+# keys_zone=single-site-with-caching.com:100m creates the memory zone and sets the maximum size in MBs.
+# inactive=60m will remove cached items that haven't been accessed for 60 minutes or more.
+fastcgi_cache_path /var/www/$domain/cache levels=1:2 keys_zone=$domain:100m inactive=60m;
+
 server {
+    # Ports to listen on, uncomment one.
     listen 443 ssl http2;
     listen [::]:443 ssl http2;
 
+    # Server name to listen for
     server_name $domain;
 
+    # Path to document root
+    root /var/www/$domain/public;
+
+    # Paths to certificate files.
     ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
-    include /etc/letsencrypt/options-ssl-nginx.conf;
-    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+    # Created by certbot. Useful ? -> To check.
+    # include /etc/letsencrypt/options-ssl-nginx.conf;
+    # ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+    
+    # File to be used as index
+    index index.php;
 
+    # Overrides logs defined in nginx.conf, allows per site logs.
     access_log /var/www/$domain/logs/access.log;
     error_log /var/www/$domain/logs/error.log;
+    
+    # Default server block rules
+	# include /etc/nginx/global/server/defaults.conf;
 
-    root /var/www/$domain/public;
-    index index.php;
+	# Fastcgi cache rules
+	include global/server/fastcgi-cache.conf;
+
+	# SSL rules
+	include global/server/ssl.conf;    
 
     location / {
         try_files \$uri \$uri/ /index.php\$is_args\$args;
@@ -46,10 +68,30 @@ server {
 		
 	location ~ \.php\$ {
         try_files \$uri =404;
-        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
-        fastcgi_pass unix:/run/php/php7.4-fpm.sock;
-        fastcgi_index index.php;
-        include fastcgi_params;
+        include global/fastcgi-params.conf;
+
+        # Use the php pool defined in the upstream variable.
+		# See global/php-pool.conf for definition.
+		fastcgi_pass   \$upstream;
+
+        # Skip cache based on rules in global/server/fastcgi-cache.conf.
+        fastcgi_cache_bypass \$skip_cache;
+        fastcgi_no_cache \$skip_cache;
+
+
+        # OLD STUFF - To delete if it works
+        # fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+        # fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+        # fastcgi_index index.php;
+        # fastcgi_cache_bypass $skip_cache;
+        # fastcgi_no_cache $skip_cache;
+
+
+        # Define memory zone for caching. Should match key_zone in fastcgi_cache_path above.
+        fastcgi_cache $domain;
+
+        # Define caching time.
+        fastcgi_cache_valid 60m;
     }
 
     location ~ /\.ht {
@@ -63,6 +105,8 @@ server {
         log_not_found off;
     }
 }
+
+# Redirect www to non-www
 server {
     listen 443 ssl http2;
     listen [::]:443 ssl http2;
@@ -75,6 +119,7 @@ server {
     return 301 https://$domain\$request_uri;
 }
 
+# Redirect http to https
 server {
     listen 80;
     listen [::]:80;
@@ -99,5 +144,6 @@ EOF
 echo "Downloading and installing Wordpress"
 cd /var/www/$domain/public
 sudo -u www-data wp core download --locale=fr_BE
-sudo -u www-data wp core config --dbname=$db_name --dbuser=$db_user --dbpass=$db_password
+sudo -u www-data wp config create --dbname=$db_name --dbuser=$db_user --dbpass=$db_password
 
+echo "Setting up server-side cron settings"
